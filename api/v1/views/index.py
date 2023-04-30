a0d48738ff68da45735f265ee057b59d1f1313cc
Introvertedtechie <zillionaire234@gmail.com>
2023-04-30 12:55:32 +0000

    soso/bayo

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 """
-This module creates a Flask Blueprint object named app_views
+Index module
 """
 
-from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from flask import jsonify
 
 
 @app_views.route('/status')
 def status():
-    """Return the status of the API"""
+    """Returns a JSON with the status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def count():
+    """Returns a JSON with the count of each object type"""
+    count_dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(count_dict)
