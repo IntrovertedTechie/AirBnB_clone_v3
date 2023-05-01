72a217988812f039aeca4fbbe319a9388472c2c5
Introvertedtechie <zillionaire234@gmail.com>
2023-05-01 08:50:50 +0000

    SB

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+This module creates a new view for Amenity objects that handles all default
+RESTful API actions
+"""
+from api.v1.views import app_views
+from models import storage, Amenity
+from flask import jsonify, abort, request
+
+
+@app_views.route('/amenities', methods=['GET'])
+def get_amenities():
+    """Retrieves the list of all Amenity objects"""
+    amenities = storage.all('Amenity').values()
+    amenities_list = [amenity.to_dict() for amenity in amenities]
+    return jsonify(amenities_list)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def get_amenity(amenity_id):
+    """Retrieves a Amenity object"""
+    amenity = storage.get('Amenity', amenity_id)
+    if not amenity:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """Deletes a Amenity object"""
+    amenity = storage.get('Amenity', amenity_id)
+    if not amenity:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/amenities', methods=['POST'])
+def create_amenity():
+    """Creates a Amenity"""
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    amenity = Amenity(**data)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def update_amenity(amenity_id):
+    """Updates a Amenity object"""
+    amenity = storage.get('Amenity', amenity_id)
+    if not amenity:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict())
+
