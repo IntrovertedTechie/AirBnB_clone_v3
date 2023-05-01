cb452a4e4ad486b5b6ac574d01306682affb6ce4
Introvertedtechie <zillionaire234@gmail.com>
2023-05-01 08:21:46 +0000

    soso/bayo

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -34,9 +34,6 @@ def get_states():
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
 
-
-
-
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
     """Retrieves a State object"""
@@ -44,7 +41,6 @@ def get_state(state_id):
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
-
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
@@ -59,15 +55,18 @@ def delete_state(state_id):
 
 @app_views.route('/states', methods=['POST'])
 def create_state():
-    """Creates a State"""
-    if not request.json:
-        abort(400, "Not a JSON")
-    if 'name' not in request.json:
-        abort(400, "Missing name")
-    state = State(**request.json)
-    state.save()
-    return jsonify(state.to_dict()), 201
+    '''Creates a State'''
+    if not request.get_json():
+        abort(400, 'Not a JSON')
 
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+
+    new_state = State(name=request.json['name'])
+    storage.new(new_state)
+    storage.save()
+
+    return jsonify(new_state.to_dict()), 201
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def update_state(state_id):
