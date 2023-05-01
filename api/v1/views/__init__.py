fd4d2a502071b31b20123a381c68f09eba888e54
Introvertedtechie <zillionaire234@gmail.com>
2023-05-01 05:25:59 +0000

    Sos
    o/Bayo

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,28 +1,7 @@
 #!/usr/bin/python3
-"""
-This module instantiates a Flask application.
-"""
-
-from api.v1.views import app_views
-from flask import Flask, jsonify
-from models import storage
-
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """Closes the database at the end of the request."""
-    storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """Handles 404 errors with a custom JSON response."""
-    return jsonify({'error': 'Not found'}), 404
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+"""create blueprint"""
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+if app_views is not None:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
