ec56bb1d2d1e99657a9be8daa4f4baeddff92eb5
Introvertedtechie <zillionaire234@gmail.com>
2023-05-01 07:41:47 +0000

    test

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,14 +2,29 @@
 """
 Defines the RESTful API actions for State objects.
 """
-
-from flask import abort, jsonify, request
-from models import storage
-from models.state import State
 from api.v1.views import app_views
+
+from flask import jsonify, abort, request
+
+from models import storage
+
+from models.state import State
+
 from datetime import datetime
+
 import uuid
 
+
+
+
+@app_views.route('/states/', methods=['GET'])
+
+def list_states():
+    """
+    Retrieves a list of all State objects
+    """
+    list_states = [obj.to_dict() for obj in storage.all("State").values()]
+    return jsonify(list_states)
 
 @app_views.route('/states', methods=['GET'])
 def get_states():
@@ -18,6 +33,8 @@ def get_states():
     """
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
+
+
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
