49409430cdb16bebd4f620e1e59ff356bff5c920
Introvertedtechie <zillionaire234@gmail.com>
2023-05-01 06:05:06 +0000

    so/ba

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,7 @@ This module defines the State view for handling RESTFul API actions.
 
 from flask import abort, jsonify, request, Blueprint
 
-from models import storage, State
+from models import storage, state
 
 
 states_api = Blueprint("states_api", __name__)
@@ -15,7 +15,7 @@ states_api = Blueprint("states_api", __name__)
 @states_api.route("/states", methods=["GET"], strict_slashes=False)
 def all_states():
     """Retrieves a list of all State objects."""
-    states = storage.all(State).values()
+    states = storage.all(state).values()
     states = [state.to_dict() for state in states]
     return jsonify(states)
 
@@ -23,7 +23,7 @@ def all_states():
 @states_api.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def single_state(state_id):
     """Retrieves a single State object by state_id."""
-    state = storage.get(State, state_id)
+    state = storage.get(state, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
@@ -37,7 +37,7 @@ def create_state():
         abort(400, "Not a JSON")
     if "name" not in data:
         abort(400, "Missing name")
-    state = State(**data)
+    state = state(**data)
     storage.new(state)
     storage.save()
     return jsonify(state.to_dict()), 201
@@ -50,7 +50,7 @@ def update_state(state_id):
     if data is None:
         abort(400, "Not a JSON")
     ignore_keys = ["id", "created_at", "updated_at"]
-    state = storage.get(State, state_id)
+    state = storage.get(state, state_id)
     if state is None:
         abort(404)
     for key, value in data.items():
@@ -63,7 +63,7 @@ def update_state(state_id):
 @states_api.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
 def delete_state(state_id):
     """Deletes an existing State object by state_id."""
-    state = storage.get(State, state_id)
+    state = storage.get(state, state_id)
     if state is None:
         abort(404)
     storage.delete(state)
